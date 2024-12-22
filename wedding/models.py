531c3a6857ba95e557ce66adf1a5090546574ee6
Andrea <andrea.barnabo92@gmail.com>
2024-12-22 12:57:57 +0100

    migrations

--- wedding/models.py
+++ wedding/models.py
@@ -25,7 +25,7 @@ class RSVP(models.Model):
 class Friend(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=50)
-    comment = models.TextField()
+    comment = models.TextField(blank=True, null=True)
     item_titles = models.CharField(max_length=255)  
     item_price = models.FloatField() 
 
